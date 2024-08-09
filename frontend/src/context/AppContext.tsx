635e2564416e7c23805d07e79f2bb3793ca5b7bc
Anshpatel2434 <anshpatel2434@gmail.com>
2024-08-09 23:44:28 +0530

    more setup

---
+++ frontend/src/context/AppContext.tsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+import React, { createContext, useEffect, useState } from "react";
+
+export interface Context {}
+export const AppContext = createContext<Context | null>(null);
+
+const AppContextProvider: React.FC<{ children: React.ReactNode }> = ({
+	children,
+}) => {
+	return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+};
+
+export default AppContextProvider;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import AppContextProvider from "./context/AppContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<AppContextProvider>
+		<App />
+	</AppContextProvider>
+);
