a06ea1603a683b6e2ac9cab71ebf5a1af0a886ee
DaVinci <siddharthkotak280@gmail.com>
2024-08-10 07:23:38 +0530

    unnecessary commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,21 @@ import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Profile from "./pages/Profile";
-
 import Pricing from "./pages/Pricing";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/virtual-tour" element={<VirtualTour />} />
-				<Route path="/signup" element={<Signup />} />
-				<Route path="/signin" element={<Signin />} />
-				<Route path="/pricing" element={<Pricing></>}></Route>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/virtual-tour" element={<VirtualTour />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/pricing" element={<Pricing />}></Route>
         <Route path="/profile" element={<Profile />} />
-			</Routes>
-		</BrowserRouter>
-	);
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
